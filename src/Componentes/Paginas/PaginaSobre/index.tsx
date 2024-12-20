7f9d75b7613902c5525d0c1f697dbab751302dd5
saulo.rodrigues <saulohrodrigues@gmail.com>
2024-12-20 17:08:35 -0300

    Refatorada a pagina sobre, aplicando responsividade - correção

--- src/Componentes/Paginas/PaginaSobre/index.tsx
+++ src/Componentes/Paginas/PaginaSobre/index.tsx
@@ -1,110 +1,154 @@
 import styled from "styled-components";
 import FotoSaulo from "../../../assets/FotoSaulo.jpeg";
 import ListSkills from "../../SoftSkills";
+import breakpoints from "../../../Styles/Breakpoint/breakpoint";
 
-const SaldacaoInicial = styled.h1`
-    font-family: 'TTSupermolot-Bold';
-    font-size: 36px;
-    font-weight: 400;
-    color: #292929;
-    width: 505px;
-    height: 44px;
-    position: fixed;
-    top: 157px; 
-    left: 124px;
-`
+const Container = styled.div`
+  display: flex;
+  flex-direction: row;
+  margin-top: 50px;
+  max-width: 1200px;
+  margin: 0 auto;
+
+  @media ${breakpoints.bg} {
+    flex-direction: column;
+    align-items: center;
+    padding: 0 20px;
+  }
+`;
+
+const SideSection = styled.div`
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+  @media ${breakpoints.bg} {
+    justify-content: center;
+    align-items: center;
+  }
+`;
+
+const MainSection = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+  position: relative;
+
+  @media ${breakpoints.bg} {
+    justify-content: center;
+    align-items: center;
+  }
+`;
 
 const Nome = styled.h1`
-    font-family: 'Roboto Flex';
-    font-size: 50px;
-    font-weight: 700;
-    color: #292929;
-    width: 364px;
-    height: 94px;
-    white-space: nowrap;
-    position: fixed;
-    top: 206px; 
-    left: 124px;
-    `
+  font-family: "Roboto Flex", sans-serif;
+  font-size: 80px;
+  font-weight: 700;
+  color: #292929;
+  margin-left: 124px;
+  margin-top: 5px;
 
-    const DercricaoProfissional = styled.h1`
-    font-family: 'TTSupermolot-Regular';
-    font-size: 36px;
-    font-weight: 400;
-    line-height: 44.28px;
-    color: #292929;
-    width: 534px;
-    height: 88px;
-    position: fixed;
-    top: 300px; 
-    left: 120px;
-    `
-    const Foto = styled.img`
-    width: auto;
-    height: 292px;
-    border-radius: 50%;
-    object-fit: cover;
-    position: fixed;
-    top: 157px; 
-    left: 757px;
-    `
+  @media ${breakpoints.bg} {
+    margin-left: 0;
+    text-align: center;
+  }
+`;
 
-    const TituloSobreMim = styled.h1`
-    font-family: 'TTSupermolot-Bold';
-    font-size: 36px;
-    font-weight: 400;
-    line-height: 44.46px;
-    color: #292929;
-    width: 176px;
-    height: 44px;
-    position: fixed;
-    top: 494px; 
-    left: 121px;
-    `
-    const TituloHabilidades = styled.h1`
-    font-family: 'TTSupermolot-Bold';
-    font-size: 36px;
-    font-weight: 400;
-    line-height: 44.46px;
-    color: #292929;
-    width: 191px;
-    height: 44px;
-    position: fixed;
-    top: 494px; 
-    left: 757px;
-    `
+const SaldacaoInicial = styled.h2`
+  font-family: 'TTSupermolot-Bold';
+  font-size: 36px;
+  font-weight: 400;
+  color: #292929;
+  margin-left: 124px;
+  margin-top: 53px;
 
-    const DesscricaoSobreMim = styled.text`
-    font-family: 'Roboto Flex';
-    font-size: 15px;
-    font-weight: 700;
-    color: #292929;
-    width: 500px;
-    height: 181px;
-    position: fixed;
-    top: 570px; 
-    left: 121px;
-   
-    `
+  @media ${breakpoints.bg} {
+    margin-left: 0;
+    text-align: center;
+  }
+`;
+
+const DercricaoProfissional = styled.h2`
+  font-family: 'TTSupermolot-Regular';
+  display: flex;
+  font-size: 36px;
+  font-weight: 400;
+  color: #292929;
+  margin-left: 120px;
+  margin-top: 2px;
+  margin-bottom: 83px;
+  
+  @media ${breakpoints.bg} {
+    margin-left: 0;
+    text-align: center;
+  }
+`;
+
+const SectionTitle = styled.h3`
+  font-family: 'TTSupermolot-Bold';
+  font-size: 36px;
+  font-weight: 400;
+  color: #292929;
+  margin-left: 121px;
+  margin-top: 36px;
+
+  @media ${breakpoints.bg} {
+    margin-left: 0;
+    text-align: center;
+  }
+`;
+
+const DesscricaoSobreMim = styled.p`
+  font-family: "Roboto Flex", sans-serif;
+  font-size: 16px;
+  letter-spacing: 1.6px;
+  font-weight: 300;
+  color: #292929;
+  width: 360px;
+  line-height: 19px;
+  margin-left: 121px;
+
+  @media ${breakpoints.bg} {
+    margin-left: 0;
+    text-align: center;
+  }
+`;
+
+const Image = styled.img`
+  width: 500px;
+  height: 500px;
+  border-radius: 50%;
+  margin-left: 116px;
+  margin-right: 116px;
+  padding-top: 45px;
+
+  @media ${breakpoints.bg} {
+    width: 100%;
+    height: auto;
+  }
+`;
     
 const PaginaSobre = () => {
     return (
-        <> 
+        <Container> 
+            <SideSection>
             <SaldacaoInicial>Olá, meu nome é </SaldacaoInicial>
             <Nome>Saulo Henrique Rodrigues</Nome>
-            <Foto src={FotoSaulo}/>
-            <DercricaoProfissional>Sou desenvolvedor de software na DBServer.</DercricaoProfissional>
-            <TituloSobreMim>Sobre mim</TituloSobreMim>
+            <DercricaoProfissional>Sou desenvolvedor full-stac.</DercricaoProfissional>
             <DesscricaoSobreMim>                
                 Sou formado técnico em processamento de dados. Posteriormente, cursei a faculdade de direito e advoguei por cinco anos.
                 Ao trabalhar como advogado, tive a percepção de que minha real paixão é ser programador, pois sinto-me mais afinado com a área.
                 Em 2022, decidi transitar (retornar) para a área de programação. A princípio, meu interesse era ser programador Back em Java. No entanto, ao ser selecionado para participar do programa de inclusão da DB, fui levado a aprender novas stecks, dentre as quais, as de FrontEnd, que me empolgam bastante. Desde então, tenho as estudado. Embora, ainda pricipiante, me sinto motivado a desbravar o universo Full Steck.
                 Nessa tônica, estou me propondo um novo projeto para 2025, nova graduação em Engennharia da Computação, não para sair da programação, mas ampliar meus conhecimentos em TI, tornar-me um programador com uma visão mais ampla, tanto na linha de tecnologia quanto de negócios.
             </DesscricaoSobreMim>
-            <TituloHabilidades>Habilidades</TituloHabilidades>
+
+            </SideSection>
+
+            <MainSection>
+                <Image src={FotoSaulo}/>
+           
             <ListSkills/>
-            
-        </>
+            </MainSection>
+        </Container>
     )
 }
 export default PaginaSobre;

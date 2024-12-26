179fd86f24e091a36e75f45e6085467cefdf0fc1
saulo.rodrigues <saulohrodrigues@gmail.com>
2024-12-26 11:30:09 -0300

    alteração do arquivo vercel, para incluir configurações de fonts

--- src/Componentes/EstiloGlobal/index.tsx
+++ src/Componentes/EstiloGlobal/index.tsx
@@ -3,12 +3,12 @@ import { createGlobalStyle } from "styled-components";
 const EstiloGlobal = createGlobalStyle`
         @font-face {
         font-family: 'TTSupermolot-Regular';
-        src: url('/src/Componentes/EstiloGlobal/Fontes/supermolot-ttf/TT-Supermolot-Regular.ttf') format('ttf');
+        src: url('./Fontes/supermolot-ttf/TT-Supermolot-Regular.ttf') format('ttf');
     }
 
     @font-face {
         font-family: 'TTSupermolot-Bold';
-        src: url('/src/Componentes/EstiloGlobal/Fontes/supermolot-ttf/TT-Supermolot-Bold.ttf');
+        src: url('./Fontes/supermolot-ttf/TT-Supermolot-Bold.ttf');
     }
 
     @import url('https://fonts.googleapis.com/css2?family=Roboto+Flex:opsz,wght@8..144,100..1000&display=swap');
